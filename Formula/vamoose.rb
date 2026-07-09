ba6597078cb548c421f20247f3015ed01a134039
Douglas Adolph <116844282+dcadolph@users.noreply.github.com>
2026-07-09 15:34:05 -0500

    Update vamoose to 0.4.3

--- Formula/vamoose.rb
+++ Formula/vamoose.rb
@@ -1,28 +1,28 @@
 class Vamoose < Formula
   desc "Calendar workflow engine for time off, approvals, and quick actions"
   homepage "https://github.com/dcadolph/vamoose"
-  version "0.4.2"
+  version "0.4.3"
   license "BUSL-1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.2/vamoose_0.4.2_Darwin_arm64.tar.gz"
-      sha256 "af3f161b00d3143fa009bb8bb77bb1db3f2e691c279a22fed95eddc31f9479af"
+      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.3/vamoose_0.4.3_Darwin_arm64.tar.gz"
+      sha256 "dbeafd3767279a646b2752405bc5b8042f3bb0b1c1421be3fc613c979f11bb99"
     end
     on_intel do
-      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.2/vamoose_0.4.2_Darwin_x86_64.tar.gz"
-      sha256 "88e0cdc8f797d8638e5b56856396fcad704a3c9189dcac9de65d9efa4823c238"
+      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.3/vamoose_0.4.3_Darwin_x86_64.tar.gz"
+      sha256 "67e8b1cb49e0b2666cd0ec22653661c70d4e0ea64413d2a27ae985715941477f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.2/vamoose_0.4.2_Linux_arm64.tar.gz"
-      sha256 "4e681b12adca2e67b84932ffcf32a099e2276d4f59a864b93fcccda7aec979d4"
+      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.3/vamoose_0.4.3_Linux_arm64.tar.gz"
+      sha256 "0867b501fbcff5044afbdcc382bc03d8395e4f0e159505989bda50e75fe0aaa2"
     end
     on_intel do
-      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.2/vamoose_0.4.2_Linux_x86_64.tar.gz"
-      sha256 "8c76da71475572024ab72af58d6a003f7b5f7180eaa4d5ba2e95c3686bddfaf8"
+      url "https://github.com/dcadolph/vamoose/releases/download/v0.4.3/vamoose_0.4.3_Linux_x86_64.tar.gz"
+      sha256 "ff99017f334121b5c1241aefbcaa07d7dfc4c97502d25c4db3b2cc47b983c8e9"
     end
   end
 
